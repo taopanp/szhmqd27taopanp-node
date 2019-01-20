c81f90375084c7df1d3449e5545078570238770d
taopanp <1349578921@qq.com>
2019-01-20 10:54:07 +0800

    完成项目的初始化

---
+++ src/app.js
@@ -0,0 +1,27 @@
+// 导包
+
+const  express=require('express')
+
+
+// 创建app
+
+const app=express()
+
+// 处理请求
+
+app.get('/',(req,res)=>{
+res.send('hello world')
+
+
+})
+
+// 启动 
+app.listen(4567,'127.0.0.1',err=>{
+if(err){
+
+    console.log(err);
+
+    
+}
+console.log('start  ok');
+})
